92225e56dab1b10e492b2fef62526c29c83c3678
Edgar Basto <edgar.pt@gmail.com>
2019-11-22 17:37:49 +0000

    adding rest framework

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', #add djangorestframework
     #apps
     'apps.endpoints'
 ]
